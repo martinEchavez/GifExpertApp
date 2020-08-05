65b9da1a67968d4b5c06c1ea2f7cc628405c957c
martinEchavez <martinechavez1994@gmail.com>
2020-08-04 21:54:33 -0500

    Docs agregado

---
+++ docs/precache-manifest.92e4eb665b4ab75540935d1141b31b3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ac9ad0f7a51c27eb0535d51b4225627",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e453a3ebe8636aee9d30",
+    "url": "/static/css/main.07a6117e.chunk.css"
+  },
+  {
+    "revision": "77e69ad879cd77aedb7f",
+    "url": "/static/js/2.da65bb96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.da65bb96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e453a3ebe8636aee9d30",
+    "url": "/static/js/main.8efd81d1.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
